d7786eedebb2dc01e49338007271813374eebf42
Orli Guy <orli.guy@s.afeka.ac.il>
2022-01-01 16:20:12 +0200

    organize

--- my-movie-frontend/src/App.js
+++ my-movie-frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
-import { Catalog } from "./components/pages/Catalog";
+import { Catalog } from "./components/catalog/Catalog";
 
 function App() {
   return (
